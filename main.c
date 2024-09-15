4758fe245038fa9c95ab471d1b5c15089c7d65b2
bad1and <163727824+bad1and@users.noreply.github.com>
2024-09-15 22:52:09 +0300

    test3

--- main.c
+++ main.c
@@ -7,11 +7,19 @@ int n_button;
 int position = 0;
 short int number1;
 short int number2;
+bool take_number1;
+bool take_number2;
 int itog;
+char *moving;
 bool f_flag = false;
 
 
-void printMenu(int position) {
+void Menu() {
+    printw("%s", "Calculator by Tikhanov Oleg\n");
+    printw("%s", "ver 0.9\n");
+    printw("%s", "-----------------\n");
+    printw("%s", "\n");
+
     char menu[8][30] = {
         "1. Ввести число А",
         "2. Ввести число Б",
@@ -29,8 +37,17 @@ void printMenu(int position) {
         } else {
             printw("%s\n", menu[i]);
         }
+        // if (i == 7 && take_number1 != true) {
+        //     printw("%s" , "Число А не введено!\n");
+        // }
+        // if (i == 7 && take_number2 != true) {
+        //     printw("%s" , "Число Б не введено!");
+        // }
+        // if (i == 7 && f_flag == true && take_number1 == true && take_number2 == true) {
+        //     printw("%s %d %s %d = %d", "Результат:", number1, moving, number2, itog);
+        // }
         if (i == 7 && f_flag == true) {
-            printw("%s %d", "Результат:", itog);
+            printw("%s %d %s %d = %d", "Результат:", number1, moving, number2, itog);
         }
     }
 }
@@ -40,7 +57,7 @@ int main() {
     initscr();
 
     while (true) {
-        printMenu(position);
+        Menu(position);
 
         n_button = getch();
         if (n_button == 27) {
@@ -70,8 +87,9 @@ int main() {
             clear();
             printw("%s", "Введите число А: ");
             scanw("%d\n", &number1);
+            take_number1 = true;
             clear();
-            printMenu(position);
+            Menu(position);
         }
 
         //Enter B
@@ -79,39 +97,44 @@ int main() {
             clear();
             printw("%s", "Введите число Б: ");
             scanw("%d\n", &number2);
+            take_number2 = true;
             clear();
-            printMenu(position);
+            Menu(position);
         }
 
         //Summ
         if ((position == 2 && n_button == 10) || n_button == 51) {
+            moving = "+";
             f_flag = true;
             itog = number1 + number2;
             clear();
-            printMenu(position);
+            Menu(position);
         }
 
         //Vichet
         if ((position == 3 && n_button == 10) || n_button == 52) {
+            moving = "-";
             f_flag = true;
             itog = number1 - number2;
             clear();
-            printMenu(position);
+            Menu(position);
         }
         //Ymnozh
         if ((position == 4 && n_button == 10) || n_button == 53) {
+            moving = "*";
             f_flag = true;
             itog = number1 * number2;
             clear();
-            printMenu(position);
+            Menu(position);
         }
 
         //del
         if ((position == 5 && n_button == 10) || n_button == 54) {
+            moving = "/";
             f_flag = true;
             itog = number1 / number2;
             clear();
-            printMenu(position);
+            Menu(position);
         }
 
         //close calc
